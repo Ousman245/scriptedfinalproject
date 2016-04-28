f13ac06e8ec6dbd7a16c1a0d3866743fe61b7781
ousman245 <ocamara@cristoreyny.org>
2016-04-28 12:58:16 +0000

    Soldier is almost there

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-<img src="https://s-media-cache-ak0.pinimg.com/736x/55/5d/a6/555da6e79a7951607bb351669f45b54d.jpg" id="myImage">
+//<img src="https://s-media-cache-ak0.pinimg.com/736x/55/5d/a6/555da6e79a7951607bb351669f45b54d.jpg
+//" id="myImage"></img>
 
 
+$(document).ready(function(){
+    var img = $("<img>");
+img.attr('src','http://bf4gp.localhostr.de/Battlefield%204/soldier/missions-soldier-1.png');
+img.css('width', '10px');
+$("body").append(img);
 
-if()
-
-$('#myImage').click(fadeOut);
-
-
+});
 
