6a85afb91ae8114790808252a88587f6bea44bc5
ousman245 <ocamara@cristoreyny.org>
2016-05-05 12:58:38 +0000

    game done

--- js/script.js
+++ js/script.js
@@ -5,8 +5,23 @@
 $(document).ready(function(){
     var img = $("<img>");
 img.attr('src','http://bf4gp.localhostr.de/Battlefield%204/soldier/missions-soldier-1.png');
-img.css('width', '10px');
+img.css('width', '100px');
 $("body").append(img);
+
+console.log('hello')
+
+var img2 = $("<img>");
+img2.attr('src','http://www.clipartbest.com/cliparts/yTk/gR8/yTkgR8r8c.png');
+img2.css('width', '100px')
+var gun = $('#Gun').offset();
+img2.css('left', gun.left - 20);
+img2.css('top', gun.top + 20);
+img2.addClass('bubble')
+
+$("body").append(img2);
+    
+
+    
 
 });
 
