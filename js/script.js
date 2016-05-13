d30e1bfee3fc1132897aa33ad9bc6a42d2b70ece
ousman245 <ocamara@cristoreyny.org>
2016-05-13 12:58:47 +0000

    done

--- js/script.js
+++ js/script.js
@@ -2,18 +2,29 @@
 //" id="myImage"></img>
 
 
+
+var soldierPosition = {
+    x: 0,
+    y: 0
+};
+
 $(document).ready(function() {
-    var img = $("<img>");
+
+    var img = $("<img>").attr('id', "soldier");
     img.attr('src', 'http://bf4gp.localhostr.de/Battlefield%204/soldier/missions-soldier-1.png');
     img.css('width', '100px');
+    img.css('position', 'absolute');
     $("body").append(img);
-
-
 
     $("body").click(createBubble);
     $("body").mousemove(gunMove);
 
+    
 });
+
+function distance(point1, point2) {
+    return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
+}
 
 function createBubble(ev) {
 
@@ -32,10 +43,50 @@ function createBubble(ev) {
         left: ev.pageX,
         width: 0
     }, 1000);
+
+    var mousePoint = {
+        x: ev.pageX,
+        y: ev.pageY
+    };
+    var dist = distance(soldierPosition, mousePoint);
+
+    if (dist < 300) {
+        $("#soldier").animate({
+            width: 0,
+            height: 0
+        }, 900);
+        newSoldier();
+    }
 };
 
+
+
 function gunMove(ev) {
-    console.log(ev);
+    //console.log(ev);
     $('#Gun').css('top', ev.pageY)
     $('#Gun').css('left', ev.pageX)
 };
+function newSoldier(){
+    var randomTop= Math.round(Math.random()*100) + 1;
+    var randomLeft= Math.round(Math.random()*100) + 1;
+    
+    soldierPosition.x = randomLeft;
+    soldierPosition.y = randomTop;
+
+    $("#soldier").css('top', randomTop + '%');
+    $("#soldier").css('left', randomLeft + '%' );
+     $("#soldier").animate({
+            width: 100,
+            height: 200
+        }, 100);
+    
+}
+SC.initialize({
+  client_id: '90f2a1fb287219fc9350fe313a7dfced'
+});
+
+
+SC.stream('/tracks/263566457').then(function(player){
+  player.play();
+});
+
